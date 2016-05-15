7d34462fc367710959fcb3b80285d11b412697b0
zhangbobo <420679828@qq.com>
2016-05-15 12:00:31 +0800

    测试10

---
+++ text10/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text10
+//
+//  Created by lanouhn on 16/5/15.
+//  Copyright © 2016年 zhangbobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text10/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text10
+//
+//  Created by lanouhn on 16/5/15.
+//  Copyright © 2016年 zhangbobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
